7a8b9822e9e801394367dbf80611472efd466aa1
Selim KURT <67548518+SelimKurtx@users.noreply.github.com>
2023-01-12 00:38:19 +0300

    s

---
+++ app/head.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+        <title>Selim KURT BLog</title>
+        <meta name='description' content='Yeni Blog sayfam NextJS' />
+        <link rel='icon' href='/favicon.ico' />
+    </>
+    
+  )
+}
+
+export default Head
